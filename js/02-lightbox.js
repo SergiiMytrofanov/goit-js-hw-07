47c00742913ab7a37afa0fb5e50fb927c7b5b5df
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-05-27 19:53:04 +0300

    task_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+// Створюємо галерею
+const galleryLigthBoxitems = galleryItems.map((item) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>
+    </li>`).join('');
+
+    gallery.insertAdjacentHTML('beforeend', galleryLigthBoxitems);
+
+// додаємо лайтбокс одним із методів офіційного сайту
+const lightbox = new SimpleLightbox('.gallery a', {
+  overlayOpacity: 0.9,
+  captionsData: 'alt',
+  preloading: true,
+  animationSpeed: 250,
+  fadeSpeed: 300,
+});
