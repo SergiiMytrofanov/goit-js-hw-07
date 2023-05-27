c008781db50bb1d3bc50540b90d05b4ee89297b3
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-05-27 07:08:53 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryItemsHTML = galleryItems.map(item => 
+    ` <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"/>
+    </a>
+  </li>`).join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryItemsHTML);
+
+function openModal(evt) {
+  evt.preventDefault();
+
+  if (evt.target.tagName !== 'IMG') {
+    return;
+  }
+ 
+  const modal = basicLightbox.create(`<img src="${evt.target.dataset.source}">`);
+
+  modal.show();
+}
+
+gallery.addEventListener('click', openModal);
+
+
+
